99755a3ded76974532eefd99060343955102b931
Fr1stn <happipampers00@gmail.com>
2025-09-12 20:01:17 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,15 @@
+Console.Write("Скорость лодки в стоячей воде (V, км/ч): ");
+double v = Convert.ToDouble(Console.ReadLine());
+Console.Write("Скорость течения реки (U, км/ч, U < V): ");
+double u = Convert.ToDouble(Console.ReadLine());
+if (u >= v)
+{
+    Console.WriteLine("Ошибка: скорость течения должна быть меньше скорости лодки!");
+    return;
+}
+Console.Write("Время движения по озеру (T1, ч): ");
+double t1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Время движения против течения реки (T2, ч): ");
+double t2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"V: {v} км/ч \nU: {u} км/ч \nT1: {t1} ч \nT2: {t2} ч");
+Console.WriteLine($"Общий путь (S, км), пройденный лодкой: {(v / t1) + ((v - u) / t2)}");
